822bed271cfbedca28aae37750879490523f3b6f
张宝冬 <baodong.zhang@argrace.ai>
2020-12-29 17:25:06 +0800

    update

--- test.js
+++ test.js
@@ -1,36 +1,14 @@
-const headPet = {
-  // 宠物总店提供了最基础的出售，回购宠物的服务
-  sellPet: (name) => {
-    // 出售宠物
-    console.log('出售一只宠物', name);
-  },
-  name: 'shop',
-};
-const subPet = new Proxy(headPet, {
-  get(target, key) {
-    if (key === 'sellPet') {
-      return {
-        sell(name) {
-          // 出售宠物
-          headPet.sellPet(name);
-        },
-        sellPetFood(name) {
-          // 购买宠物食物送宠物活动
-          console.log('搭配了一大波食粮');
-          headPet.sellPet(name);
-        },
-        sellPetCage(name) {
-          // 购买宠物笼子送宠物活动
-          console.log('搭配一个大笼子');
-          headPet.sellPet(name);
-        },
-      };
-    } else {
-      return target[key];
-    }
-  },
-});
-subPet.sellPet.sellPetFood('tom');
-console.log(subPet.name);
-
-
+function f1() {
+  var n = 999;
+  nAdd = function () {
+    n += 1;
+  };
+  function f2() {
+    console.log(n);
+  }
+  return f2;
+}
+var result = f1();
+result(); // 999
+nAdd();
+result(); // 1000
