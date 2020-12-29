a37b755c8ca165ff25ebc5f594a4dc228e6d2e1c
张宝冬 <baodong.zhang@argrace.ai>
2020-12-29 21:43:50 +0800

    update

--- test.js
+++ test.js
@@ -1,14 +1,11 @@
-function f1() {
-  var n = 999;
-  nAdd = function () {
-    n += 1;
+function trigger(fun, wait) {
+  let timer;
+  return function (args) {
+    if (!timer) {
+      fun(args);
+      timer = setTimeout(() => {
+        timer = null;
+      }, wait);
+    }
   };
-  function f2() {
-    console.log(n);
-  }
-  return f2;
 }
-var result = f1();
-result(); // 999
-nAdd();
-result(); // 1000
