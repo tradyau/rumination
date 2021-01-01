14c8e80588743a1d1334acf6172a0a2603b7c268
张宝冬 <baodong.zhang@argrace.ai>
2021-01-02 00:54:35 +0800

    update

--- Vue/响应式/MVue.js
+++ Vue/响应式/MVue.js
@@ -1,3 +1,55 @@
+const compileUtil = {
+  // 根据key获取真正的value
+  getValue(expr, vm) {
+    return expr.split('.').reduce((data, currentValue) => {
+      return data[currentValue];
+    }, vm.$date);
+  },
+  text(node, expr, vm) {
+    let value;
+    //   判断{{}}的情况
+    if (expr.indexOf('{{') !== -1) {
+      // 正则匹配，去掉大括号，取到真正的key，再去获取data中的实际值
+      value = expr.replace(/\{\{(.+?)\}\}/g, (...args) => {
+        return this.getValue(args[1], vm);
+      });
+    } else {
+      // 从vm的$data中取到对应的值,通过getValue取到真正的值（解决data中多层对象的嵌套。data.person.fav）
+      value = this.getValue(expr, vm);
+    }
+    // 更新视图
+    this.updater.textUpdater(node, value);
+  },
+  html(node, expr, vm) {
+    const value = this.getValue(expr, vm);
+    this.updater.htmlUpdater(node, value);
+  },
+  model(node, expr, vm) {
+    const value = this.getValue(expr, vm);
+    this.updater.modelUpdater(node, value);
+  },
+  // 事件
+  on(node, expr, vm, eventName) {
+    // 根据事件名称获取methods里对应的回调
+    let fn = vm.$options.methods && vm.$options.methods[expr];
+    // 绑定时间，同时绑定回调方法的this指向vm实例
+    node.addEventListener(eventName, fn.bind(vm), false);
+  },
+  // 绑定属性
+  bind() {},
+  // 更新数据的方法
+  updater: {
+    textUpdater(node, value) {
+      node.textContent = value;
+    },
+    htmlUpdater(node, value) {
+      node.innerHTML = value;
+    },
+    modelUpdater(node, value) {
+      node.value = value;
+    },
+  },
+};
 class Compile {
   constructor(el, vm) {
     // 判断是html代码，还是节点对象
@@ -51,10 +103,48 @@ class Compile {
     // 获取元素节点上的属性
     const attributes = node.attributes;
     [...attributes].forEach((attr) => {
-      console.log(attr);
+      // 结构赋值，拿到属性的名称和属性值
+      const { name, value } = attr;
+      // 判断属性是否是指令属性
+      if (this.isDirectName(name)) {
+        // 此时的name为：v-text v-html v-model v-on:click
+        // 结构赋值拿到“v-”后的指令内容,此时的directive为：text html model on:click
+        const [, directive] = name.split('-');
+        // on:click需要进一步处理，继续结构赋值
+        // 此时，dirName为：text html model on eventName的值为：click
+        const [dirName, eventName] = directive.split(':');
+        // 策略模式，针对不同的属性，执行不同的操作，更新视图
+        // node: 当前的元素节点
+        // value: 属性值
+        // vm: 当前实例，主要是为了获取data里的数据
+        // eventName: 触发的事件名称（v-on:click）
+        compileUtil[dirName](node, value, this.vm, eventName);
+        // 删除有指令的标签上的属性
+        node.removeAttribute('v-' + directive);
+      } else if (this.isEventName(name)) {
+        // 处理@click
+        let [, eventName] = name.split('@');
+        compileUtil['on'](node, value, this.vm, eventName);
+      }
     });
   }
-  compileText(node) {}
+  compileText(node) {
+    // 使用正则匹配{{}}
+    const content = node.textContent;
+    if (/\{\{(.+?)\}\}/.test(content)) {
+      compileUtil['text'](node, content, this.vm);
+    }
+  }
+
+  // 判断属性是否是指令属性
+  isDirectName(attrName) {
+    return attrName.startsWith('v-');
+  }
+
+  // 判断是否是@指令属性
+  isEventName(attrName) {
+    return attrName.startsWith('@');
+  }
 }
 
 class MVue {
--- test.js
+++ test.js
@@ -1,11 +1,12 @@
-function trigger(fun, wait) {
-  let timer;
-  return function (args) {
-    if (!timer) {
-      fun(args);
-      timer = setTimeout(() => {
-        timer = null;
-      }, wait);
-    }
-  };
-}
+let arr = ['aa', 'bb', 'cc', 'dd', 'ee'];
+let arr2 = [1, 2, 3, 4];
+let result = '';
+console.log(
+  
+);
+arr.reduce((data, current) => {
+  console.log(data);
+  result += current;
+  return data;
+}, arr2);
+console.log(result);
